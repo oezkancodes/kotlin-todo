47fd341cbde78560662c4f6ab2cb7d5cb6cf2fdb
Özkan Yanikbas <oezkan.yanikbas@re-lounge.com>
2022-05-26 11:26:25 +0200

    Add basic auth

--- app/src/main/java/com/example/kotlintodo/MainActivity.kt
+++ app/src/main/java/com/example/kotlintodo/MainActivity.kt
@@ -2,10 +2,48 @@ package com.example.kotlintodo
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import com.example.kotlintodo.databinding.ActivityLoginBinding
+import com.example.kotlintodo.databinding.ActivityMainBinding
+import com.google.android.gms.tasks.OnCompleteListener
+import com.google.firebase.auth.AuthResult
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val email = binding.email
+        val password = binding.password
+        val login = binding.login
+
+        login.setOnClickListener {
+            login(email.text.toString(), password.text.toString())
+        }
+    }
+
+    private fun login (email: String, password: String) {
+        FirebaseAuth
+            .getInstance()
+            .signInWithEmailAndPassword(email, password)
+            .addOnCompleteListener(
+                OnCompleteListener<AuthResult> { task ->
+                    if (task.isSuccessful) {
+                        val user: FirebaseUser = task.result!!.user!!
+                        println("LOGIN SUCCESSFUL")
+                        println("USER: " + user.email)
+                    } else {
+                        println("LOGIN ERROR")
+                    }
+                }
+            )
     }
 }
