8ed290838fbca155e2545a31e1b21f86f908fd1a
Özkan Yanikbas <oezkanyanikbas@gmail.com>
2022-05-31 20:38:41 +0200

    Improve MainActivity

--- app/src/main/java/com/example/kotlintodo/MainActivity.kt
+++ app/src/main/java/com/example/kotlintodo/MainActivity.kt
@@ -2,6 +2,7 @@ package com.example.kotlintodo
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import com.example.kotlintodo.databinding.ActivityMainBinding
 import com.google.android.gms.tasks.OnCompleteListener
 import com.google.firebase.auth.AuthResult
@@ -28,7 +29,21 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
+    /**
+     * Login user with E-Mail and Password.
+     */
     private fun login (email: String, password: String) {
+        /**
+         * Basic input validation
+         */
+        if (email.isEmpty() && password.isEmpty()) {
+            Toast.makeText(this,  "Please enter E-Mail and Password.", Toast.LENGTH_SHORT).show()
+            return
+        }
+
+        /**
+         * Firebase Authentication
+         */
         FirebaseAuth
             .getInstance()
             .signInWithEmailAndPassword(email, password)
@@ -40,6 +55,7 @@ class MainActivity : AppCompatActivity() {
                         println("USER: " + user.email)
                     } else {
                         println("LOGIN ERROR")
+                        Toast.makeText(this,  "Invalid Login Data. Check E-Mail and Password.", Toast.LENGTH_SHORT).show()
                     }
                 }
             )
