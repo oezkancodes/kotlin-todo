f21f2569935900bf788c93f1589d004b2d0a2e8c
flying-turtle001 <justin.esposito@gmx.de>
2022-06-24 16:06:40 +0200

    adjust todo detail layout

--- app/src/main/java/com/example/kotlintodo/TodoDetailActivity.kt
+++ app/src/main/java/com/example/kotlintodo/TodoDetailActivity.kt
@@ -1,12 +1,32 @@
 package com.example.kotlintodo
 
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
+import android.util.Log
+import android.widget.EditText
 import androidx.appcompat.app.AppCompatActivity
 
 class TodoDetailActivity : AppCompatActivity() {
+    private lateinit var etAddStep: EditText
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         setContentView(R.layout.activity_todo_detail)
+
+        etAddStep = findViewById(R.id.etAddStep)
+
+        etAddStep.addTextChangedListener(object: TextWatcher {
+            override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {}
+
+            override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {}
+
+            override fun afterTextChanged(p0: Editable?) {
+                Log.d("Add Step Value:", "afterTextChanged $p0")
+            }
+
+        })
+
     }
 }
