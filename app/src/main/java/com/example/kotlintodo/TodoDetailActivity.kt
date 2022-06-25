025ed90ad3b1ce1410a255a970542a27a8f00902
Özkan Yanikbas <oezkanyanikbas@gmail.com>
2022-06-25 15:23:49 +0200

    Load data from Firebase

--- app/src/main/java/com/example/kotlintodo/TodoDetailActivity.kt
+++ app/src/main/java/com/example/kotlintodo/TodoDetailActivity.kt
@@ -7,6 +7,7 @@ import android.util.Log
 import android.widget.EditText
 import androidx.appcompat.app.AppCompatActivity
 import com.example.kotlintodo.databinding.ActivityTodoDetailBinding
+import com.example.kotlintodo.model.Step
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.firestore.ktx.firestore
 import com.google.firebase.ktx.Firebase
@@ -21,14 +22,17 @@ class TodoDetailActivity : AppCompatActivity() {
          * Get uid from intent.putExtra of previous Activity.
          */
         uid = intent.extras?.get("uid") as String
+        loadTodo(uid)
 
         /**
          * Setup Binding & View
+         * TODO: rename binding IDs after UI is finished
          */
         super.onCreate(savedInstanceState)
         binding = ActivityTodoDetailBinding.inflate(layoutInflater)
         setContentView(binding.root)
         val backButton = binding.btnBackToList
+
         val important = binding.favoriteStar
         val done = binding.cbCheckTodo
 
@@ -58,7 +62,17 @@ class TodoDetailActivity : AppCompatActivity() {
             .get()
             .addOnSuccessListener { docRef ->
                 val data: MutableMap<String, Any> = docRef.data as MutableMap<String, Any>
-
+                val label = data["label"] as String
+                val note = data["note"] as String
+                // TODO: set steps after UI is ready
+                // val steps = data["steps"] as MutableList<Step>
+                val done = data["done"] as Boolean
+                val important = data["important"] as Boolean
+                println(label)
+                binding.cbCheckTodo.setText(label)
+                binding.etNotes.setText(note)
+                binding.cbCheckTodo.isChecked = done
+                binding.favoriteStar.isChecked = important
             }
     }
 }
